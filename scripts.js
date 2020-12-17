91949190398a092810a1c985e323b145710f3824
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2020-12-16 21:29:42 -0500

    added input element

--- scripts.js
+++ scripts.js
@@ -8,32 +8,44 @@ $(document).ready(function () {
   for (hour = 9; hour <= 17; hour++) {
     var index = hour - 9;
     var rowDivEl = $("<div>");
-    rowDivEl.addClass('row');
-    rowDivEl.addClass('plannerRow');
-    rowDivEl.attr('hour-index', hour);
+    rowDivEl.addClass("row");
+    rowDivEl.addClass("plannerRow");
+    rowDivEl.attr("hour-index", hour);
 
-    var timeBox = $('<div>');
-    timeBox.addClass('col-md-2');
+    var timeColEl = $("<div>");
+    timeColEl.addClass("col-3");
 
-    var timeBoxSpan = $('<span>');
-    timeBoxSpan.attr('class','timeBox');
+    var timeBoxSpan = $("<span>");
+    timeBoxSpan.attr("class", "timeBox");
 
     var displayTime = 0;
     var ampm = "";
     if (hour > 12) {
-        displayTime = hour - 12;
-        ampm = "pm";
+      displayTime = hour - 12;
+      ampm = "pm";
     } else {
-        displayTime = hour;
-        ampm = "am";
+      displayTime = hour;
+      ampm = "am";
     }
 
-    timeBoxSpan.text(displayTime+" "+ampm);
+    timeBoxSpan.text(displayTime + " " + ampm);
     console.log(timeBoxSpan);
 
+    timeColEl.append(timeBoxSpan);
+    rowDivEl.append(timeColEl);
     $("#main").append(rowDivEl);
-    rowDivEl.append(timeBox);
-    timeBox.append(timeBoxSpan);
+
+    var inputEl = $("<input>");
+    inputEl.attr("id", "input-" + index);
+    inputEl.attr("hour-index", index);
+    inputEl.attr("type", "text");
+    inputEl.attr("class", "dailyPlan");
+
+    var inputColEl = $("<div>");
+    inputColEl.addClass("<col>");
+
+    inputColEl.append(inputEl);
+    rowDivEl.append(inputColEl);
   }
   // Function Definitions
   // Function Calls
