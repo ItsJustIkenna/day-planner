0cfc10ab0fe9e37152135c868428a8b6081d5816
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2020-12-17 19:39:46 -0500

    updated README.md, added local storage, and dynamic css

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # day-planner
+
+This assignment generates a random password from the criteria selected from the prompts. It features dynamically updating HTML and CSS powered by JavaScript code. The user is prompted to input the length of the password, if the password will include uppercase letters and lowercase letters, numeric values and special characters then the code generates a password based on those criteria and displays it on the screen.
+
+## Credits
+
+Special shoutout to Jeana Rose Mathis for help with this homework. Here is the [link](https://github.com/jeanarose) to her GitHub
+
+## Screenshot
+
+![screenshot.PNG](screenshot.PNG)
+
+## link to website
+
+https://itsjustikenna.github.io/day-planner/
+
--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   var main = $("#main");
   var dateHeading = $("#currentDay");
   // JavaScript Variables
-  var now = moment();
+  var now = moment().format("dddd, MMMM, do YYYY");
   dateHeading.text(now);
 
   timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -14,14 +14,25 @@ $(document).ready(function () {
     var btnIcon = $("<i>");
     var timeBoxSpanEl = $("<span>");
     var textBoxEl = $("<textarea>");
-    
-    textBoxEl.addClass("col-md-10 description past");
+
+    textBoxEl.attr("id", timeArr[hour]);
+    textBoxEl.addClass("col-md-10 description");
+    if (moment().hour() === timeArr[hour]) {
+        textBoxEl.addClass("present");
+    } else if (moment().hour() > timeArr[hour]) {
+        textBoxEl.addClass("past");
+    } else if (moment().hour() < timeArr[hour]) {
+        textBoxEl.addClass("future");
+    }
+
+    var saveData = localStorage.getItem(timeArr[hour]);
+    textBoxEl.text(saveData);
 
     rowEl.addClass("row time-block");
 
     //display the time dynamically
     var displayHour = moment().set("hour", timeArr[hour]).format("hA");
-    
+
     //give time element content
     timeBoxSpanEl.addClass("col-md-1");
     timeBoxSpanEl.text(displayHour);
@@ -31,22 +42,24 @@ $(document).ready(function () {
 
     //give button element content
     btnEl.addClass("saveBtn col-md-1");
+    btnEl.attr("data-anything", timeArr[hour]);
     btnIcon.addClass("far fa-save");
     btnEl.append(btnIcon);
 
     //append all columns to the row
     rowEl.append(timeBoxSpanEl, textBoxEl, btnEl);
-    
+
     //append row to container
     main.append(rowEl);
 
-    console.log(timeBoxSpanEl[0].innerHTML);
-    console.log(textBoxEl);
-    console.log(btnEl);
   }
 
   // Function Definitions
   // Function Calls
   // Event Listeners
-  $(".saveBtn").on("click", function () {});
+  $(".saveBtn").on("click", function (event) {
+    var key = $(event.target).attr("data-anything");
+    var value = $('#'+key).val();
+    localStorage.setItem(key, value);
+  });
 });
